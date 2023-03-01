754d84320d27d95f8ca029cdca3719ba9c2d6644
Janis Ringli <janis.ringli@gmail.com>
2023-03-01 16:08:45 +0100

    design of weather acivity done

---
+++ app/src/main/java/ch/jr/rainready/WeatherActivity.java
@@ -0,0 +1,12 @@
+package ch.jr.rainready;
+
+import android.os.Bundle;
+
+public class WeatherActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather_main);
+    }
+}
